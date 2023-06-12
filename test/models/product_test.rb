d4f1aef05a6ed72e1fa7581f7dbf3b48e7fde204
Alex <murinskii@gmail.com>
2023-06-12 16:59:09 +0500

    tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+end
+
+test
+  "product is not valid without a unique title" do
+  product = Product.new(:title => products(:ruby).title,
+  :description => "yyy",
+  :price => 1,
+  :image_url => "fred.gif")
+  assert !product.save
+  assert_equal "has already been taken", product.errors[:title].join('; ')
 end
